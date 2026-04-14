1f5afcaae1db48129e1ecab552829e7badf64b9d
AngiesRoom <angelomorara123@gmail.com>
2026-04-14 13:06:51 +0300

    Update Bus.java

--- src/Bus.java
+++ src/Bus.java
@@ -1,21 +1,21 @@
-public class Bus extends Vehicle {
-    public Bus(String make, int year) {
-        super(make, year);
+public class Bus extends Vehicle{
+    public Bus(String name, int year){
+        super(name, year);
     }
 
     @Override
-    public void accelerate(double speed) {
-        System.out.println(make + " accelerates to " + speed + " km/h");
+    public void accelerate(int speed){
+        System.out.println("The bus has accelerated by: " + speed + " km/hr");
+    }
+    @Override
+    public void stop(){
+        System.out.println("The bus has stopped");
+    }
+    @Override
+    public void gas(){
+        System.out.println("Gas has been applied");
     }
 
-    @Override
-    public void stop() {
-        System.out.println(make + " has stopped.");
-    }
 
-    @Override
-    public double gas() {
-        return 100.0;
-    }
+
 }
-
