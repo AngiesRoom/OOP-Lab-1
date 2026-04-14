a475b51a978a814396a388f6d6cd655dbc827eb4
AngiesRoom <angelomorara123@gmail.com>
2026-04-14 13:08:21 +0300

    Update SportsCar.java

--- src/SportsCar.java
+++ src/SportsCar.java
@@ -1,43 +1,45 @@
-public class SportsCar extends Vehicle implements Automobile {
+public class SportsCar extends Vehicle implements Automobile{
+    public int doors;
+    public int maxSpeed ;
+    public double engineSize;
 
-    public int    doors;
-    public double fuelLevel;
-
-
-    public SportsCar(String make, int year) {
-        super(make, year);
-        this.doors       = 2;
-        this.fuelLevel   = MAX_FUEL;
-
-    }
-
-
-    @Override
-    public void accelerate(double speed) {
-        fuelLevel -= speed * 0.08;
-        System.out.println(make + " (" +   speed + " km/h  ⛽ " + fuelLevel + "%");
+    public SportsCar(String name, int year, int doors, int maxSpeed, double engineSize){
+        super(name, year);
+        this.doors = doors;
+        this.maxSpeed = maxSpeed;
+        this.engineSize = engineSize;
     }
 
     @Override
-    public void stop() {
-        System.out.println(make + " has stopped.");
+    public void accelerate(int speed){
+        System.out.println("The " + name + " has accelerated by: " + speed + " km/hr");
     }
-
+    public void accelerate(){
+        System.out.println("The vehicle has not accelerated");
+    }
     @Override
-    public double gas() {
-        return fuelLevel;
+    public void stop(){
+        System.out.println("The " + name + " has stopped");
     }
-
-
     @Override
-    public void brake() {
-        System.out.println(make + " brakes aggressively.");
+    public void gas(){
+        System.out.println("Gas has been applied");
+    }
+    @Override
+    public void displayMaxSpeed(){
+        System.out.println("The max speed of the " + name + " is " + maxSpeed);
+    }
+    @Override
+    public void displayDoors(){
+        System.out.println("The number of doors is " + doors);
+    }
+    @Override
+    public void displayEngineSize(){
+        System.out.println("The engine size is " + engineSize);
     }
 
-    @Override
-    public String getInfo() {
-        return "SportsCar: " + year + " " + make +
-                " | Doors: " + doors +
-                " | Fuel: " + fuelLevel + "%";
-    }
+
+
+
+
 }
